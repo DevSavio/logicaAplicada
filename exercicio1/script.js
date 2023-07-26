13a02c75d7fee384f9a36de978d1f0fdca6b6563
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-26 15:39:44 -0300

    repartindo responsabilidades

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -7,11 +7,24 @@ formular.addEventListener('submit', function(event){
     const lado1 = parseFloat(document.getElementById('lado1').value);
     const lado2 = parseFloat(document.getElementById('lado2').value);
 
-    if (isNaN(lado1) || isNaN(lado2) || lado1 <= 0 || lado2 <= 0) {
-        resultado.textContent = 'Por favor, insira valores válidos para largura e comprimento.';
+    if (verificaValores(lado1, lado2)) {
+        resultado.textContent = 'Por favor, verifique a inserção dos seus dados.';
     } else {
-        const area = lado1 * lado2;
-        resultado.textContent = `A área do terreno é ${area} m².`;
+        const area = calcular(lado1, lado2);
     }
 
 });
+
+verificarValores = function(valor1, valor2) {
+    if (isNaN(valor1) || isNaN(valor2) || valor1 <= 0 || valor2 <= 0) {
+        resultado.textContent = 'Por favor, insira valores válidos para largura e comprimento.';
+    }
+}
+
+calcular = function(value1, value2) {
+    return value1 * value2;
+}
+
+result = function(area) {
+    return resultado.textContent = `A área do terreno é ${area} m².`;
+}
