f69db65e13e44df29c7cb243b1bd129737441242
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-27 15:29:47 -0300

    Adicionado icones para cada exercicio no projeto

--- exercicio1/exercicio1_angular/exercico1/src/app/app.component.ts
+++ exercicio1/exercicio1_angular/exercico1/src/app/app.component.ts
@@ -23,102 +23,102 @@ export class AppComponent {
         {
           title: 'Exercício 1',
           link: 'exercicio1',
-          //icon: 'shopping-bag-outline',
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 2',
           link: 'exercicio2',
-          //icon: 'shopping-cart-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 3',
           // link: 'ponto_venda/list',
-          //icon: 'pin-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 4',
           // link: 'familia_produto/list',
-          //icon: 'radio-button-on-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 5',
           // link: 'pessoa/list',
-          //icon: 'people-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 6',
           // link: 'categoria/list',
-          //icon: 'grid-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 7',
           // link: 'fornecedor/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 8',
           // link: 'unidade_medida/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 9',
           // link: 'fornecedor/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 10',
           // link: 'unidade_medida/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 11',
           // link: 'local_estoque/list',
-          //icon: 'shopping-bag-outline',
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 12',
           // link: 'produto/list',
-          //icon: 'shopping-cart-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 13',
           // link: 'ponto_venda/list',
-          //icon: 'pin-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 14',
           // link: 'familia_produto/list',
-          //icon: 'radio-button-on-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 15',
           // link: 'pessoa/list',
-          //icon: 'people-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 16',
           // link: 'categoria/list',
-          //icon: 'grid-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 17',
           // link: 'fornecedor/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 18',
           // link: 'unidade_medida/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 19',
           // link: 'fornecedor/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
         {
           title: 'Exercício 20',
           // link: 'unidade_medida/list',
-          //icon: 'car-outline'
+          icon: 'hash-outline',
         },
       ],
     },
