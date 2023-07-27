494a1129c5006802a59bd3941404c6ad852d3096
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-27 14:38:59 -0300

    Implementado a lógica do primeiro exercicio

--- exercicio1/exercicio1_angular/exercico1/src/app/pages/exercicio1/exercicio1-list/exercicio1-list.component.ts
+++ exercicio1/exercicio1_angular/exercico1/src/app/pages/exercicio1/exercicio1-list/exercicio1-list.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-exercicio1-list',
   templateUrl: './exercicio1-list.component.html',
-  styleUrls: ['./exercicio1-list.component.css']
+  styleUrls: ['./exercicio1-list.component.css'],
 })
 export class Exercicio1ListComponent {
+  largura!: number;
+  comprimento!: number;
+  resultado: number | null = null;
 
+  verificaValores(largura: number, comprimento: number) {
+    if (this.largura == undefined && this.comprimento == undefined) {
+      alert('Digite valores válidos para o calculo');
+    }
+  }
+
+  calcularArea() {
+    if (this.largura !== undefined && this.comprimento !== undefined) {
+      this.resultado = this.largura * this.comprimento;
+    } else {
+      this.resultado = null;
+    }
+  }
 }
