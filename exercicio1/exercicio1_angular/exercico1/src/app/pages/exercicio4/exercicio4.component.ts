c64b02542e40fb7f8af1223d27cfe08df8f63bf7
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-31 10:49:47 -0300

    Implementado o componente 4 com sua logica e o visual

--- exercicio1/exercicio1_angular/exercico1/src/app/pages/exercicio4/exercicio4.component.ts
+++ exercicio1/exercicio1_angular/exercico1/src/app/pages/exercicio4/exercicio4.component.ts
@@ -1,54 +1,40 @@
-// import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-exercicio4',
-//   templateUrl: './exercicio4.component.html',
-//   styleUrls: ['./exercicio4.component.css']
-// })
-// export class Exercicio4Component {
-//   nome!: string;
-//   nascimento!: Date;
-//   resultadoA: number | null = null;
-//   resultadoI: number | null = null;
+@Component({
+  selector: 'app-exercicio4',
+  templateUrl: './exercicio4.component.html',
+  styleUrls: ['./exercicio4.component.css'],
+})
+export class Exercicio4Component implements OnInit {
+  nome!: string;
+  nascimento: Date | null = null;
+  dataAtual!: Date;
+  totalDeDias: number | null = null;
 
-//   private camposSaoValidos(): boolean {
-//     return this.nome !== undefined && this.nascimento !== undefined;
-//   }
+  constructor() {}
 
-//   private realizarMultiplicacaoA(): number | null {
-//     return this.camposSaoValidos()
-//       ? this.pao * 0.35 && this.pao_de_queijo * 0.8 && this.broa * 1.5
-//       : null;
-//   }
+  ngOnInit(): void {
+    this.dataAtual = new Date();
+  }
 
-//   calcularDiasDeVida() {
-//     if (this.camposForamAlterados()) {
-//       this.resultadoA = this.realizarMultiplicacaoA();
-//     }
-//   }
+  private realizarContagem(nascimento: Date, dataAtual: Date) {
+    if (nascimento && dataAtual) {
+      const diffTime = Math.abs(
+        this.dataAtual.getTime() - this.nascimento!.getTime()
+      );
+      this.totalDeDias = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    } else {
+      this.totalDeDias = null;
+    }
+  }
 
-//   private realizarMultiplicacaoI(): number | null {
-//     return (this.resultadoI = this.resultadoA! * 0.1);
-//   }
+  calcularDiasDeVida() {
+    this.realizarContagem(this.nascimento!, this.dataAtual);
+  }
 
-//   calcularAnosBisextos() {
-//     if (this.camposForamAlterados()) {
-//       this.resultadoI = this.realizarMultiplicacaoI();
-//     }
-//   }
-
-//   private camposForamAlterados(): boolean {
-//     return this.pao !== undefined && this.broa !== undefined;
-//   }
-
-//   limpar() {
-//     this.nome = "";
-//     this.nascimento = '';
-//     this.resultadoA = null;
-//   }
-
-//   limparI() {
-//     this.resultadoA = null;
-//     this.resultadoI = null;
-//   }
-// }
+  limpar() {
+    this.nome = '';
+    this.nascimento = null;
+    this.totalDeDias = null;
+  }
+}
