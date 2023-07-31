60bacbb24ef94a09d2abcd71a8b866d49613875a
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-31 10:50:19 -0300

    Adicionado o componente exercicio 5  com sua logica e o visual

---
+++ exercicio1/exercicio1_angular/exercico1/src/app/pages/exercicio5/exercicio5.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio5',
+  templateUrl: './exercicio5.component.html',
+  styleUrls: ['./exercicio5.component.css'],
+})
+export class Exercicio5Component {
+  valor: number | null = null;
+  combustivel: number | null = null;
+  totalLitros: number | null = null;
+
+  private camposSaoValidos(): boolean {
+    return this.valor !== undefined && this.combustivel !== undefined;
+  }
+
+  private realizarCalculo(valor: number, combustivel: number): number | null {
+    return this.camposSaoValidos() ? valor / combustivel : null;
+  }
+
+  calcularLitros() {
+    this.realizarCalculo(this.valor!, this.combustivel!);
+  }
+
+  limpar() {
+    this.valor = null;
+    this.combustivel = null;
+    this.totalLitros = null;
+  }
+}
