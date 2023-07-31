e849f036c50c543c76a2c1a89feec04c92fca508
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-31 10:49:10 -0300

    adicionado o modulo de calendario e adicionado nas declarações os componentes exercicio 4 e 5

--- exercicio1/exercicio1_angular/exercico1/src/app/app.module.ts
+++ exercicio1/exercicio1_angular/exercico1/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   NbButtonModule,
   NbCardModule,
+  NbDatepickerModule,
   NbIconModule,
   NbInputModule,
   NbLayoutModule,
@@ -21,7 +22,8 @@ import { FormsModule } from '@angular/forms';
 import { Exercicio1ListComponent } from './pages/exercicio1/exercicio1-list.component';
 import { Exercicio2Component } from './pages/exercicio2/exercicio2.component';
 import { Exercicio3Component } from './pages/exercicio3/exercicio3.component';
-// import { Exercicio4Component } from './pages/exercicio4/exercicio4.component';
+import { Exercicio4Component } from './pages/exercicio4/exercicio4.component';
+import { Exercicio5Component } from './pages/exercicio5/exercicio5.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,8 @@ import { Exercicio3Component } from './pages/exercicio3/exercicio3.component';
     Exercicio1ListComponent,
     Exercicio2Component,
     Exercicio3Component,
-    // Exercicio4Component,
+    Exercicio4Component,
+    Exercicio5Component,
   ],
   imports: [
     CommonModule,
@@ -46,6 +49,7 @@ import { Exercicio3Component } from './pages/exercicio3/exercicio3.component';
     BrowserAnimationsModule,
     NbButtonModule,
     NbInputModule,
+    NbDatepickerModule.forRoot(),
   ],
   providers: [NbMenuService],
   bootstrap: [AppComponent],
