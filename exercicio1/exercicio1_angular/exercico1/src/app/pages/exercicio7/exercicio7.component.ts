6c9feec01c710a33284e566d450c7603f993ddff
SAVIO DE SOUZA <log.savio@gitlab.com>
2023-07-31 16:53:16 -0300

    Criado o componente 7 ao projeto

---
+++ exercicio1/exercicio1_angular/exercico1/src/app/pages/exercicio7/exercicio7.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio7',
+  templateUrl: './exercicio7.component.html',
+  styleUrls: ['./exercicio7.component.css']
+})
+export class Exercicio7Component {
+
+}
